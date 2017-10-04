50fa65998124e0ac4f7f8bf93de6bbbf7bd58dc5
Vytautas Mickus <vmickus@gmx.com>
2017-10-04 08:46:17 +0300

    Cleanup app crypto.cpp

--- app/crypto.cpp
+++ app/crypto.cpp
@@ -84,15 +84,16 @@ static void RSA_get0_key(const RSA *r,
 }
 #endif
 
+#define SSL_CHECK(X) \
+    if(!(X)){ \
+        ERR_print_errors_fp(stdout);    \
+        throw runtime_error("SSL error");        \
+    }
 EC_KEY* to_ec_key(sgx_ec256_private_t* prv_key)
 {
     EC_KEY *ec_key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
     BIGNUM *bn_prv_r = BN_lebin2bn(prv_key->r, 32, 0);
-    if(1 != EC_KEY_set_private_key(ec_key, bn_prv_r))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Crap");
-    }
+    SSL_CHECK(1 == EC_KEY_set_private_key(ec_key, bn_prv_r));
 
     return ec_key;
 }
@@ -101,53 +102,46 @@ EC_KEY* to_ec_key(sgx_ec256_public_t* pub_key)
     EC_KEY *ec_key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
     EC_GROUP *curve = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
 
-    BIGNUM *prv = BN_new();
     EC_POINT *pub = EC_POINT_new(curve);
 
     BIGNUM *bn_pub_x = BN_lebin2bn(pub_key->gx, 32, 0);
     BIGNUM *bn_pub_y = BN_lebin2bn(pub_key->gy, 32, 0);
 
-    if(1 != EC_POINT_set_affine_coordinates_GFp(curve, pub, bn_pub_x, bn_pub_y, 0))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Crap");
-    }
+    SSL_CHECK(1 == EC_POINT_set_affine_coordinates_GFp(curve, pub, bn_pub_x, bn_pub_y, 0));
 
-    EC_KEY_set_public_key(ec_key, pub);
-    ERR_print_errors_fp(stdout);
+    SSL_CHECK(1 == EC_KEY_set_public_key(ec_key, pub));
+
+    EC_GROUP_free(curve);
     return ec_key;
 }
 
 void ec256_gen_key(sgx_ec256_private_t *prv_key, sgx_ec256_public_t *pub_key) {
     EC_GROUP *curve;
 
-    if (NULL == (curve = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1)))
-        throw runtime_error("Crap");
+    SSL_CHECK(NULL != (curve = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1)));
 
     EC_KEY *key;
 
-    if (NULL == (key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)))
-        throw runtime_error("Crap");
+    SSL_CHECK(NULL != (key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)));
 
-    if (1 != EC_KEY_generate_key(key))
-        throw runtime_error("Crap");
+    SSL_CHECK(1 == EC_KEY_generate_key(key));
 
-    BIGNUM *prv = BN_new();
+    BIGNUM *prv = (BIGNUM*)EC_KEY_get0_private_key(key);
     BIGNUM *pub_x = BN_new();
     BIGNUM *pub_y = BN_new();
 
-    prv = (BIGNUM*)EC_KEY_get0_private_key(key);
     const EC_POINT* pub_p = EC_KEY_get0_public_key(key);
 
-    if(1 != EC_POINT_get_affine_coordinates_GFp(curve, pub_p, pub_x, pub_y, 0))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Crap");
-    }
+    SSL_CHECK(1 == EC_POINT_get_affine_coordinates_GFp(curve, pub_p, pub_x, pub_y, 0));
 
     BN_bn2lebinpad(prv, (uint8_t*)prv_key, 32);
     BN_bn2lebinpad(pub_x, (uint8_t*)pub_key->gx, 32);
     BN_bn2lebinpad(pub_y, (uint8_t*)pub_key->gy, 32);
+
+    EC_GROUP_free(curve);
+    EC_KEY_free(key);
+    BN_free(pub_x);
+    BN_free(pub_y);
 }
 
 ec256_dhkey* get_shared_dhkey(sgx_ec256_private_t* prv_key, sgx_ec256_public_t* peer_key)
@@ -189,11 +183,7 @@ void aes128_decrypt(uint8_t *key, uint8_t *data, uint32_t data_size, gcm_tag_t *
                       data_size);
 
     // authentication step
-    if(1 != EVP_DecryptFinal(ctx, out_data+n_processed, &n_processed))
-    {
-        ERR_print_errors_fp(stderr);
-        throw runtime_error("Failure");
-    }
+    SSL_CHECK(1 == EVP_DecryptFinal(ctx, out_data+n_processed, &n_processed));
     EVP_CIPHER_CTX_free(ctx);
 }
 
@@ -204,34 +194,18 @@ void aes128_encrypt(uint8_t *key, const uint8_t *data, uint32_t data_size, uint8
     int n_processed = 0;
 
     /* Initialise the encryption operation. */
-    if(1 != EVP_EncryptInit(ctx, EVP_aes_128_gcm(), key, iv))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Failure");
-    }
+    SSL_CHECK(1 == EVP_EncryptInit(ctx, EVP_aes_128_gcm(), key, iv));
 
     //EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 12, NULL); // default: 12
 
     // Additional Authenticated Data (AAD)
     // EVP_EncryptUpdate(ctx, NULL, &n_processed, 0, 0);
 
-    if(1 != EVP_EncryptUpdate(ctx, out_data, &n_processed, data, data_size))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Failure");
-    }
+    SSL_CHECK(1 == EVP_EncryptUpdate(ctx, out_data, &n_processed, data, data_size));
 
-    if(1 != EVP_EncryptFinal_ex(ctx, out_data + n_processed, &n_processed))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Failure");
-    }
+    SSL_CHECK(1 == EVP_EncryptFinal_ex(ctx, out_data + n_processed, &n_processed));
 
-    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, out_mac))
-    {
-        ERR_print_errors_fp(stdout);
-        throw runtime_error("Failure");
-    }
+    SSL_CHECK(1 == EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, out_mac));
 
     EVP_CIPHER_CTX_free(ctx);
 }
@@ -239,16 +213,23 @@ void aes128_encrypt(uint8_t *key, const uint8_t *data, uint32_t data_size, uint8
 bool ec256_check_point(sgx_ec256_public_t *pub_key) {
     BN_CTX *ctx = BN_CTX_new();
 
-    EC_KEY *ec_key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
     EC_GROUP *curve = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
 
-    BIGNUM *prv = BN_new();
     EC_POINT *pub = EC_POINT_new(curve);
 
     BIGNUM *bn_pub_x = BN_lebin2bn(pub_key->gx, 32, 0);
     BIGNUM *bn_pub_y = BN_lebin2bn(pub_key->gy, 32, 0);
 
-    return 1 == EC_POINT_set_affine_coordinates_GFp(curve, pub, bn_pub_x, bn_pub_y, ctx);
+    bool ret = 1 == EC_POINT_set_affine_coordinates_GFp(curve, pub, bn_pub_x, bn_pub_y, ctx);
+
+    BN_free(bn_pub_x);
+    BN_free(bn_pub_y);
+
+    EC_POINT_free(pub);
+    EC_GROUP_free(curve);
+    BN_CTX_free(ctx);
+
+    return ret;
 }
 
 AES_GCM_msg ec256_encrypt_msg(sgx_ec256_public_t* pub_key, ec256_dhkey* sk_key, const buffer &msg_data) {
